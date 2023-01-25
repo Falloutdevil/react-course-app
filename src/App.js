48a87e08d7a982319468de9520a18bbbf8f8a7dc
Falloutdevil <Falloutdevil@ya.ru>
2023-01-25 23:54:39 +0300

    add component Costs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import {CostItem} from './components/CostItem';
+
+import {Costs} from './components/Costs';
 
 
 function App() {
@@ -24,9 +25,7 @@ function App() {
     return (
         <div>
             <h1>Начнём React</h1>
-            <CostItem date={costs[0].date} description={costs[0].description} amount={costs[0].amount}/>
-            <CostItem date={costs[1].date} description={costs[1].description} amount={costs[1].amount}/>
-            <CostItem date={costs[2].date} description={costs[2].description} amount={costs[2].amount}/>
+            <Costs costs={costs}/>
         </div>
     );
 }
